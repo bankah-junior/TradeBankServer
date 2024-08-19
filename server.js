61b94e2bfe4df48935ada1cb9b43633d60267119
Bankah Anthony Bekoe <154749514+bankah@users.noreply.github.com>
2024-08-19 23:48:06 +0000

    cors

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const mongoose = require("mongoose");
 
 const usersRoutes = require("./routes/usersRoute");
 const tradeHistory = require("./routes/tradeHistoryRoute");
-//const cors = require('cors');
+const cors = require('cors');
 
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
-/*app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5174', 'https://trade-bank-client.vercel.app/'],
-}));*/
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:5174', 'https://trade-bank-client.vercel.app']
+}));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
